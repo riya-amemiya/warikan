836592990d522afd5a55b623658b65020e70c366
Riya <hiyoriorria@gmail.com>
2021-05-12 15:33:19 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { FCC } from 'fcc_typescript';
+import './App.scss';
+import { useState } from 'react';
+import SEO from './SEO';
+const App = () => {
+    const [numberPeople, setNumberPeople] = useState(0);
+    const [money, setMoney] = useState(0);
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        switch (e.target.name) {
+            case 'money':
+                setMoney(Number(e.target.value));
+                break;
+            case 'Number_of_people':
+                setNumberPeople(Number(e.target.value));
+                break;
+        }
+    };
+    return (
+        <div className="App" style={{ width: '100%', margin: '0px auto' }}>
+            <SEO
+                title="割り勘計算機"
+                description="割り勘計算サイト"
+                keywords="割り勘 計算 無料 わりかん けいさん"
+                // eslint-disable-next-line no-restricted-globals
+                siteUrl={location.href}
+                siteName="割り勘"
+            />
+            <header>
+                <h1>割り勘計算機</h1>
+            </header>
+            <main>
+                <div style={{ width: '10rem', margin: '0px auto' }}>
+                    <p>金額</p>
+                    <input type="text" onChange={onChange} name="money" style={{ width: '10rem', margin: '0px auto' }} />
+                </div>
+                <div style={{ width: '10rem', margin: '0px auto' }}>
+                    <p>人数</p>
+                    <input
+                        type="text"
+                        onChange={onChange}
+                        name="Number_of_people"
+                        style={{ width: '10rem', margin: '0px auto' }}
+                    />
+                </div>
+                <div>
+                    {Number.isNaN(numberPeople) && Number.isNaN(money) ? (
+                        <p>error</p>
+                    ) : (
+                        <>
+                            <p>1人あたり{FCC.Maths.quotient(money, numberPeople).answer}</p>
+                            <p>余り{FCC.Maths.quotient(money, numberPeople).surplus}</p>
+                        </>
+                    )}
+                </div>
+            </main>
+            <footer>
+                <div>
+                    <a href="https://twitter.com/manyuta06" style={{ padding: '20px' }}>
+                        <i className="fab fa-twitter fa-2x white"></i>
+                    </a>
+                    <a href="https://github.com/riya81" style={{ padding: '20px' }}>
+                        <i className="fab fa-github fa-2x white"></i>
+                    </a>
+                </div>
+            </footer>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/SEO.tsx
@@ -0,0 +1,46 @@
+import { Helmet } from 'react-helmet';
+const SEO = ({
+    title,
+    description,
+    keywords,
+    siteUrl,
+    siteName,
+}: {
+    title: string;
+    description: string;
+    keywords: string;
+    siteUrl: string;
+    siteName: string;
+}) => {
+    return (
+        <Helmet>
+            <html lang="ja"></html>
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta name="keywords" content={keywords} />
+            <meta itemProp="name" content={siteName} />
+            <link rel="canonical" href={siteUrl} />
+            <meta property="og:site_name" content={title} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:url" content={siteUrl} />
+            <meta property="og:locale" content="ja_JP" />
+            <meta property="og:type" content="website" />
+            <link
+                rel="stylesheet"
+                href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
+                integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
+                crossOrigin="anonymous"></link>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-LT883YWQRK"></script>
+            <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+            <meta name="google-site-verification" content="cjjcWBnEoVL7p0wuvHywaedD6h-3O0B2seFntNkt0jk" />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://blissful-swartz-9aac04.netlify.app/" />
+            <meta property="twitter:title" content="暇人のブログ" />
+            <meta property="twitter:description" content="暇な高校生が緩く更新するサイト" />
+            <meta property="twitter:image" content="" />
+        </Helmet>
+    );
+};
+export default SEO;
